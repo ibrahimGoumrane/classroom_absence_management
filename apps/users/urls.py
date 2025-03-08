63b088751db6fac516435d59afb72dd28ac70be4
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-03-08 22:57:08 +0000

    fix url pattern

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,6 +8,6 @@ router.register(r'', UserViewSet)
 
 # URLs
 urlpatterns = [
-    path('/', include(router.urls)),
-    path('/api/folder', FolderCreateView.as_view(), name='folder-create'),
+    path('', include(router.urls)),
+    path('api/folder', FolderCreateView.as_view(), name='folder-create'),
 ]
