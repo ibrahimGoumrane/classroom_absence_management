95186a9fbd77d129dff2c0a77b6eb43e8b1a66bd
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-03-08 19:40:44 +0000

    Add view and url for folder creation, starting #2

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
-# Router
+from .views import UserViewSet, FolderCreateView
+
+# Router for User CRUD
 router = DefaultRouter()
 router.register(r'', UserViewSet)
 
 # URLs
 urlpatterns = [
     path('/', include(router.urls)),
+    path('/api/folder', FolderCreateView.as_view(), name='folder-create'),
 ]
