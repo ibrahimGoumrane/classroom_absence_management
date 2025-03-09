6bb7ee1e4f9c55ef94cc29110745f5ce5bb584f4
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-03-09 00:11:04 +0000

    Add upload student images view and url

--- apps/students/urls.py
+++ apps/students/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet
+from .views import StudentViewSet, UploadStudentImagesView
 # Router
 router = DefaultRouter()
 router.register(r'', StudentViewSet)
 
 # URLs
 urlpatterns = [
-    path('/', include(router.urls)),
+    path('', include(router.urls)),
+    path('api/folder/images', UploadStudentImagesView.as_view(), name='upload-images'),
 ]
