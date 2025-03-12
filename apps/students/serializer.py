1c1580ffeb7d1aa3c99762006e9db33f4a019848
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-03-12 17:33:13 +0000

    Refactor ClassViewSet permissions and update URL routing;

--- apps/students/serializer.py
+++ apps/students/serializer.py
@@ -4,14 +4,18 @@ from apps.users.serializer import UserSerializer
 
 
 class StudentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = UserSerializer()
     class Meta:
         model = Student
         fields = '__all__'
-        
-        
+        extra_kwargs = {'user': {
+            'password': {'write_only': True},
+        }}
+
 class StudentImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentImage
         fields = '__all__'
+
+ 
         
