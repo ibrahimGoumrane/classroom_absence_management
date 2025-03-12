3a27034368b680270bb1ae80197a97e3fc7e5d67
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-03-12 19:43:52 +0000

    verifing the crud functionnality of the attendance class , registing  , deleting , updating , getting the attendance records

--- apps/attendance/views.py
+++ apps/attendance/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from  rest_framework import viewsets
 from .models import Attendance
 from .serializer import AttendanceSerializer
+from rest_framework.permissions import IsAuthenticated , AllowAny
+from apps.users.permissions import IsTeacher , IsAdmin
 # Create your views here.
 
 class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:  # Allow anyone to view teachers
+            return [AllowAny()]
+        return [IsAuthenticated(), IsTeacher() or IsAdmin()]  # ✅ Fixed instantiation
