9dcbb66b51c88a11eb532feb0c8f3c933f55362a
Arch <wbhaoui@gmail.com>
2025-04-04 17:59:24 +0000

    Add /departments/{id}/teachers endpoint to retrieve teachers of a specific department

--- apps/departments/views.py
+++ apps/departments/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import AllowAny
 from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from apps.teachers.serializer import TeacherSerializer
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
@@ -38,4 +39,18 @@ class DepartmentViewSet(viewsets.ModelViewSet):
         """
         queryset = Department.objects.annotate(teacherCount=Count('teachers'))
         serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'], url_path='teachers')
+    def get_teachers_for_department(self, request, pk=None):
+        """
+        Returns the list of teachers belonging to the given department.
+        """
+        try:
+            department = Department.objects.get(pk=pk)
+        except Department.DoesNotExist:
+            return Response({'detail': 'Department not found.'}, status=404)
+
+        teachers = department.teachers.all()
+        serializer = TeacherSerializer(teachers, many=True)
         return Response(serializer.data)
