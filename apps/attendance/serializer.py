2e83618864249114325f34ceb3ef9d77e4b3f714
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-05-13 10:20:54 +0100

    Refactor AttendanceSerializer to simplify student and subject fields

--- apps/attendance/serializer.py
+++ apps/attendance/serializer.py
@@ -3,22 +3,12 @@ from .models import Attendance
 from apps.students.serializer import StudentSerializer
 from apps.subjects.serializer import SubjectSerializer
 
+
 class AttendanceSerializer(serializers.ModelSerializer):
-    student_details = serializers.SerializerMethodField(read_only=True)
-    subject_details = serializers.SerializerMethodField(read_only=True)
-    
+    student = StudentSerializer(read_only=True)
+    subject = SubjectSerializer(read_only=True)
+
     class Meta:
         model = Attendance
-        fields = "__all__"
+        fields = '__all__'
 
-    def get_student_details(self, obj):
-        return {
-            'id': obj.student.id,
-            'name': f"{obj.student.user.firstName} {obj.student.user.lastName}"
-        }
-    
-    def get_subject_details(self, obj):
-        return {
-            'id': obj.subject.id,
-            'name': obj.subject.name
-        }
