f87cd525d04a5795cafbdf5cf93ec5302681fa0b
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-06-09 16:11:57 +0100

    Add students/subjects/classes count endpoints for teacher

--- apps/teachers/urls.py
+++ apps/teachers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TeacherViewSet , get_teacher_attendance ,get_teacher_subjects
+from .views import TeacherViewSet , get_teacher_attendance ,get_teacher_subjects, get_teacher_total_students, get_teacher_total_subjects
 # Router
 router = DefaultRouter()
 router.register(r'', TeacherViewSet)
@@ -10,4 +10,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('<int:id>/subjects/', get_teacher_subjects, name='teacher-subjects'),
     path('<int:id>/attendance/', get_teacher_attendance, name='teacher-attendance'),
+    path('<int:id>/total/students/', get_teacher_total_students, name='teacher-total-students'),
+    path('<int:id>/total/subjects/', get_teacher_total_subjects, name='teacher-total-subjects'),
+    path('<int:id>/total/classes/', get_teacher_total_subjects, name='teacher-total-classes'),
 ]
